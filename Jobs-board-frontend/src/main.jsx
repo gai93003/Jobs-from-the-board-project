807e2baf078c149cc4a4ac896419b08f4547d8a0
Gabriel <deng93003@gmail.com>
2025-11-28 11:49:29 +0000

    Made an error while resolving the conflict, now solved

--- Jobs-board-frontend/src/main.jsx
+++ Jobs-board-frontend/src/main.jsx
@@ -4,4 +4,7 @@ import './components/auth/signup.css';
 import SignUp from './components/auth/signup.jsx';
 
 createRoot(document.getElementById('root')).render(
-    <SignUp />
+    <>
+        <SignUp />
+    </>
+)
